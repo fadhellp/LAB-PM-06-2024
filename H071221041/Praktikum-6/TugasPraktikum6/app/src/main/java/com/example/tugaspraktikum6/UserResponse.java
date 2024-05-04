1095f02cafb8c3aafc6531779e8dd95f65b890de
andimuthia <andimuthiamlptr@gmail.com>
2024-05-04 12:57:44 +0800

    mengumpul tugas praktikum 6

---
+++ H071221041/Praktikum-6/TugasPraktikum6/app/src/main/java/com/example/tugaspraktikum6/UserResponse.java
@@ -0,0 +1,12 @@
+package com.example.tugaspraktikum6;
+
+import java.util.ArrayList;
+import java.util.List;
+public class UserResponse {
+
+    private ArrayList<User> data;
+
+    public ArrayList<User> getData() {
+        return data;
+    }
+}
